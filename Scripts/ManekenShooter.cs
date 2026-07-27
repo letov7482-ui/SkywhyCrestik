99f85ba4958c241bf38a4ac8004afb8110d64585
letov7482-ui <letov7482@gmail.com>
2026-07-27 18:14:25 +0300

    Update ManekenShooter.cs

--- Scripts/ManekenShooter.cs
+++ Scripts/ManekenShooter.cs
@@ -1,6 +1,46 @@
+using Godot;
+using System;
+
+public partial class ManekenShooter : Node3D
+{
+    [Export] public PackedScene manekenPrefab; 
+    private int score = 0;
+    private Camera3D playerCamera;
+
+    public override void _Ready()
+    {
+        playerCamera = GetNode<Camera3D>("Player/Camera3D");
+        SpawnNewManeken();
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventScreenTouch touchEvent && touchEvent.Pressed)
+        {
+            ShootRay(touchEvent.Position);
+        }
+    }
+
+    private void ShootRay(Vector2 touchPosition)
+    {
+        var from = playerCamera.ProjectRayOrigin(touchPosition);
+        var to = from + playerCamera.ProjectRayNormal(touchPosition) * 100f;
+        
+        var spaceState = GetWorld3D().DirectSpaceState;
+        var query = PhysicsRayQueryParameters3D.Create(from, to);
+        var result = spaceState.IntersectRay(query);
+
+        if (result.Count > 0 && result["collider"].As<Node3D>().IsInGroup("manekens"))
+        {
+            result["collider"].As<Node3D>().QueueFree();
+            score++;
+            GetNode<AudioManager>("/root/AudioManager").PlayShot(); 
+            SpawnNewManeken(); 
+        }
+    }
+
     private void SpawnNewManeken()
     {
-        // БЕЗОПАСНЫЙ ФИКС: Создаем цилиндр-мишень прямо кодом
         var newManeken = new CsgCylinder3D();
         newManeken.Radius = 0.4f;
         newManeken.Height = 1.8f;
@@ -11,7 +51,6 @@
         newManeken.Position = new Vector3(randomX, 0.9f, randomZ);
         newManeken.AddToGroup("manekens");
         
-        // Создаем статическое тело для обработки попаданий луча (Raycast)
         var staticBody = new StaticBody3D();
         var collisionShape = new CollisionShape3D();
         var capsuleShape = new CapsuleShape3D();
@@ -21,7 +60,8 @@
         
         staticBody.AddChild(collisionShape);
         newManeken.AddChild(staticBody);
-        staticBody.AddToGroup("manekens"); // Чтобы код луча понимал, что это цель
+        staticBody.AddToGroup("manekens"); 
 
         AddChild(newManeken);
     }
+}
